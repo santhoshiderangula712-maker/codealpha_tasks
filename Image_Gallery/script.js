f18d2029c47483d7bc8fa27e2c70a1185d21f6c2
DERANGULA NAGA SANTHOSHI <santhoshiderangula712@gmail.com>
2026-06-12 12:56:01 +0530

    Update script.js

--- Image_Gallery/script.js
+++ Image_Gallery/script.js
@@ -1,1 +1,45 @@
+// Lightbox
 
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.getElementById("close");
+
+images.forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    lightbox.style.display = "none";
+
+});
+
+// Filter Function
+
+function filterSelection(category){
+
+    const items = document.querySelectorAll(".image");
+
+    items.forEach(item => {
+
+        if(category === "all"){
+            item.style.display = "block";
+        }
+        else if(item.classList.contains(category)){
+            item.style.display = "block";
+        }
+        else{
+            item.style.display = "none";
+        }
+
+    });
+
+}
